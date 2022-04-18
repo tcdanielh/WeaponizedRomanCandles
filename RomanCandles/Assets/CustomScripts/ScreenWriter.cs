22f1dd146142504d62ca5fe9183f6b036f294bc8
Albert Wen <52183157+AlbertScribblenaut@users.noreply.github.com>
2022-04-18 14:58:53 -0700

    started adding in physical parameters for numerical solving

--- RomanCandles/Assets/CustomScripts/ScreenWriter.cs
+++ RomanCandles/Assets/CustomScripts/ScreenWriter.cs
@@ -31,10 +31,11 @@ public class ScreenWriter : MonoBehaviour
 
     public ComputeBuffer smokeBuffer;
     public ComputeBuffer ejectaBuffer;
-    public ComputeBuffer hashBuffer;
+    public ComputeBuffer hashBuffer
 
     public struct Ejecta{
         public Vector3 pos;
+        public Vector3 v;
         public Vector4 color;
     }
 
@@ -54,6 +55,7 @@ public class ScreenWriter : MonoBehaviour
         smokeBuffer = new ComputeBuffer(smokeData.Length, sizeof(float));
         smokeBuffer.SetData(smokeData);
 
+        // initialize ejecta
         Ejecta[] es = new Ejecta[10];
         for (int i = 0; i < es.Length; i++)
         {
@@ -113,6 +115,31 @@ public class ScreenWriter : MonoBehaviour
         material.SetFloat("smokeLightAbsorb", smokeLightAbsorb);;
 
         Graphics.Blit(source, destination, material);
+
+    }
+    
+    private void firework(Ejecta[] es)
+    {   /* Physical Parameters */
+        double g = 9.81;    // gravitational acceleration [m/s]
+        double eta = 0.05;  // explosive efficiency
+        double mu_a = 1.8e-5;   // air viscosity [kg/(m*s)]
+        double N_p = es.length;
+        double H_e = 3e6;   // explosive heat of combustion [J/kg]
+        double R_p = 0.005; // projectile radius
+        double R_r = .2;    // firework radius
+        double rho_a = 1.225;   // air density [kg/m^3]
+        double rho_p = 2000;    // projectile density [kg/m^3]
+        Vector3 v_a = new Vector3(0.);  // air velocity [m/s]
+        double m_s = 2; // inert structural mass [kg]
+        double m_i = 4. / 3. * MATH.PI * Math.pow(R_p, 3) * rho_p;  // mass of ejecta
+        double m_p = N_p * m_i; // total mass of ejecta
+
+        double m_e = 5; // explosive charge mass [kg]
+        double deltav = Math.sqrt(2 * eta * H_e * m_e / m_p);   // change in speed after detonation
+        double m_f = 10;    // launch charge mass [kg]
+        Vector3 vt0 = new Vector3(0., 0., (double)Math.sqrt(2 * eta * H_e * m_f / m_r));    // initial velocity [m/s]
+
+        double m_r = m_p + m_e + m_s;   // total rocket mass [kg]
 
     }
 }
