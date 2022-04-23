9d0d8bc841f1e59d328d70fa1c51707654acb162
NicholasJJ <nicholasjohnjennings@gmail.com>
2022-04-23 13:36:23 -0700

    Finish baseline fragment shader

--- RomanCandles/Assets/CustomScripts/ScreenWriter.cs
+++ RomanCandles/Assets/CustomScripts/ScreenWriter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ScreenWriter : MonoBehaviour
 {
+    public Color smokeColor;
     public float SmokeCellSideLength;
     public Vector3 SmokeGridDimensions;
 
@@ -28,16 +29,22 @@ public class ScreenWriter : MonoBehaviour
     [SerializeField] List<Transform> tempEjecta;
 
     [SerializeField] float ejectaLightIntensity;
-
+    public Color ejectaColor;
     public ComputeBuffer smokeBuffer;
     public ComputeBuffer ejectaBuffer;
     public ComputeBuffer hashBuffer;
+    
+
+    public Transform sun;
+    private Vector3 sunDir;
+    private Vector4 sunColor;
+    private float sunIntensity;
 
     public struct Ejecta{
         public Vector3 pos;
-        public Vector3 v;
+        //public Vector3 v;
         public Vector4 color;
-        public bool landed = false;
+        //public bool landed;
     }
 
 
@@ -47,6 +54,8 @@ public class ScreenWriter : MonoBehaviour
     private FireworkSim fireworkSim;
     private void Start()
     {
+        
+
         smokeSim = GetComponent<SmokeSim>();
         fireworkSim = GetComponent<FireworkSim>();
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -68,8 +77,10 @@ public class ScreenWriter : MonoBehaviour
             e.color = Random.ColorHSV();
             e.color.w = 0f;
             e.pos = Random.insideUnitSphere * 10;
+            e.pos.y = Mathf.Abs(e.pos.y);
             es[i] = e;
         }
+        foreach (Ejecta e in es) Debug.Log(e.pos);
         ejectaBuffer = new ComputeBuffer(es.Length, EjectaSize);
         ejectaBuffer.SetData(es);
 
@@ -80,17 +91,31 @@ public class ScreenWriter : MonoBehaviour
         binsPerAxis = new int[] { Mathf.CeilToInt(SmokeGridDimensions.x / hashBinSideLength), Mathf.CeilToInt(SmokeGridDimensions.y / hashBinSideLength), Mathf.CeilToInt(SmokeGridDimensions.y / hashBinSideLength) };
     }
 
+    private void Update()
+    {
+        sunColor = sun.GetComponent<Light>().color;
+        sunDir = -sun.forward;
+        sunIntensity = sun.GetComponent<Light>().intensity;
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         //Ejecta Hashing
-        hashBuffer.SetData(new Ejecta[hashBuffer.count]);
+        //hashBuffer.SetData(new Ejecta[hashBuffer.count]);
+        RenderTexture hash = new RenderTexture(binsPerAxis[0], binsPerAxis[1], 0);
+        hash.volumeDepth = binsPerAxis[2];
+        hash.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        hash.enableRandomWrite = true;
+        hash.Create();
+        EjectaHasher.SetTexture(0, "hash", hash);
+
         EjectaHasher.SetInts("binsPerAxis", binsPerAxis);
         EjectaHasher.SetVector("gridMin", gridMinPoint);
         EjectaHasher.SetVector("gridSize", SmokeGridDimensions);
         EjectaHasher.SetFloat("binLength", hashBinSideLength);
         EjectaHasher.SetInt("binSize", EjectaPerBin);
         EjectaHasher.SetBuffer(0, "Ejectas", ejectaBuffer);
-        EjectaHasher.SetBuffer(0, "Hash", hashBuffer);
+        //EjectaHasher.SetBuffer(0, "Hash", hashBuffer);
         EjectaHasher.Dispatch(0, ejectaBuffer.count / 10, 1, 1);
 
         //Debug Hash
@@ -107,6 +132,11 @@ public class ScreenWriter : MonoBehaviour
             material = new Material(shader);
         }
 
+        material.SetTexture("EHash", hash);
+        material.SetVector("binsPerAxis", new Vector4(binsPerAxis[0], binsPerAxis[1], binsPerAxis[2], 0));
+        material.SetVector("gridMin", gridMinPoint);
+        material.SetVector("gridSize", SmokeGridDimensions);
+        material.SetFloat("binLength", hashBinSideLength);
         material.SetFloat("smokeScale", scale);
         material.SetFloat("lRadius", lRadius);
         material.SetVector("BoundsMin", container.position - container.localScale / 2);
@@ -116,102 +146,110 @@ public class ScreenWriter : MonoBehaviour
         material.SetInt("numSteps", 20);
         material.SetInt("numStepsLight", 20);
         material.SetVector("lPos", lightPoint.position);
-        material.SetVector("lColor", lightPoint.GetComponent<Light>().color);
-        material.SetFloat("lIntensity", lightPoint.GetComponent<Light>().intensity);
-        material.SetFloat("smokeLightAbsorb", smokeLightAbsorb);;
+        material.SetVector("lColor", ejectaColor);
+        material.SetFloat("lIntensity", ejectaLightIntensity);
+        material.SetFloat("smokeLightAbsorb", smokeLightAbsorb);
+
+        //Sun stuff
+        material.SetVector("sunColor", sunColor);
+        material.SetVector("sunDir", sunDir);
+        material.SetFloat("sunIntensity", sunIntensity);
+        material.SetVector("smokeColor", smokeColor);
 
         Graphics.Blit(source, destination, material);
+
+        hash.Release();
 
     }
     
     // we could also include parameters for angles in spherical coordinates
-    private void firework(Ejecta[] es)
-    {   /* Physical Parameters */
-        float g = 9.81f;    // gravitational acceleration [m/s]
-        float eta = 0.05f;  // explosive efficiency
-        float mu_a = 1.8e-5f;   // air viscosity [kg/(m*s)]
-        float N_p = es.Length;
-        float H_e = 3e6f;   // explosive heat of combustion [J/kg]
-        float R_p = 0.005f; // projectile radius
-        float R_r = .2f;    // firework radius
-        float rho_a = 1.225f;   // air density [kg/m^3]
-        float rho_p = 2000.f;    // projectile density [kg/m^3]
-        Vector3 v_a = new Vector3(0,0,0);  // air velocity [m/s]
-        float m_s = 2.f; // inert structural mass [kg]
-        
-
-        // options to change burst radius, flight time, accuracy
-        float m_e = 5.f; // explosive charge mass [kg]
-        float m_f = 10.f;    // launch charge mass [kg]
-        float t_e = 5.f; // time of explsion [sec]
-        float dt = 0.001f;  // time step [sec]
-
-        float m_i = 4.f / 3.f * Mathf.PI * Mathf.Pow((float)R_p, 3) * rho_p;  // mass of ejecta 
-        float m_p = N_p * m_i; // total mass of ejecta
-        float A_px = Mathf.PI * Mathf.pow((float)R_p, 2);  // cross-sectional area of ejecta
-        
-        float m_r = m_p + m_e + m_s;   // total rocket mass [kg]
-        Vector3 v_0 = new Vector3(0, 0, Mathf.Sqrt((float)(2 * eta * H_e * m_f / m_r)));    // initial velocity [m/s]
-
-        /* Rocket Ascent */
-        // TODO: mark time for when rocket is launched
-
-        Vector3 F_gr = new Vector3(0., 0., -g * m_r);   // force of gravity on rocket [N]
-        foreach (Ejecta e in es)
-        {
-            e.v = v_0;  // initial velocity at time = 0
-        }
-        int num_steps_ascent = t_e / dt; 
-        float[] discrete_time_ascent = new float[num_steps_ascent];
-        for (int i = 0; i < num_steps_ascent; i++)
-        {
-            discrete_time_ascent[i] = dt * (float) (i-1);
-        }
-        Vector3 F_drag = new Vector3(0.); 
-        Vector3 psi_tot = new Vector3d(0.);
-        // time stepping loop during ascent
-        foreach (float dt_i in discrete_time_ascent) 
-        {
-            foreach (Ejecta e in es)
-            {
-                F_drag = 0.5f * Mathf.PI * Mathf.pow(R_r, 2) * coeff_drag(R_r, rho_a, e.v, v_a, mu_a) * 
-                    (v_a - e.v).magnitude *  * (v_a - e.v);
-                psi_tot = F_drag + F_gr;
-                e.v = e.v + dt / m_r * psi_tot;
-                e.pos = e.pos + dt * e.v;
-
-                // TODO: use fixedUpdate method
-            }
-
-        }
-
-        /* EXPLOSION!!! */
-        Vector3 blast_origin = es[0].pos;
-        float deltav = Mathf.Sqrt((float)(2 * eta * H_e * m_e / m_p));   // change in speed after detonation
-
-
-    }
-    
-    private float coeff_drag(float radius, float rho_a, Vector3 v, Vector3 v_a, float mu_a)
-    {
-        float cd;
-        float Re = (2f * radius * rho_a * (v - v_a).magnitude) / mu_a;
-        if (Re > 2e6f)
-        {
-            cd = 0.18;
-        } else if (Re <= 2e6f && Re > 3e5f)
-        {
-            cd = 3.66e-4f * Mathf.pow(Re, 0.4275f);
-        } else if (Re <= 3e5f & Re > 400.f)
-        {
-            cd = 0.5f;
-        } else if (Re <= 400f && Re > 1f)
-        {
-            cd = 24f * Mathf.pow(Re, -.646f);
-        } else if (Re < 1f)
-        {
-            cd = 24.f / Re;
-        }
-        return cd;
-    }
+    //private void firework(Ejecta[] es)
+    //{   /* Physical Parameters */
+    //    float g = 9.81f;    // gravitational acceleration [m/s]
+    //    float eta = 0.05f;  // explosive efficiency
+    //    float mu_a = 1.8e-5f;   // air viscosity [kg/(m*s)]
+    //    float N_p = es.Length;
+    //    float H_e = 3e6f;   // explosive heat of combustion [J/kg]
+    //    float R_p = 0.005f; // projectile radius
+    //    float R_r = .2f;    // firework radius
+    //    float rho_a = 1.225f;   // air density [kg/m^3]
+    //    float rho_p = 2000;    // projectile density [kg/m^3]
+    //    Vector3 v_a = new Vector3(0,0,0);  // air velocity [m/s]
+    //    float m_s = 2; // inert structural mass [kg]
+    //    
+    //
+    //    // options to change burst radius, flight time, accuracy
+    //    float m_e = 5; // explosive charge mass [kg]
+    //    float m_f = 10;    // launch charge mass [kg]
+    //    float t_e = 5; // time of explsion [sec]
+    //    float dt = 0.001f;  // time step [sec]
+    //
+    //    float m_i = 4 / 3 * Mathf.PI * Mathf.Pow((float)R_p, 3) * rho_p;  // mass of ejecta 
+    //    float m_p = N_p * m_i; // total mass of ejecta
+    //    float A_px = Mathf.PI * Mathf.Pow((float)R_p, 2);  // cross-sectional area of ejecta
+    //    
+    //    float m_r = m_p + m_e + m_s;   // total rocket mass [kg]
+    //    Vector3 v_0 = new Vector3(0, 0, Mathf.Sqrt((float)(2 * eta * H_e * m_f / m_r)));    // initial velocity [m/s]
+    //
+    //    /* Rocket Ascent */
+    //    // TODO: mark time for when rocket is launched
+    //
+    //    Vector3 F_gr = new Vector3(0., 0., -g * m_r);   // force of gravity on rocket [N]
+    //    foreach (Ejecta e in es)
+    //    {
+    //        //e.v = v_0;  // initial velocity at time = 0
+    //    }
+    //    int num_steps_ascent = Mathf.FloorToInt(t_e / dt); 
+    //    float[] discrete_time_ascent = new float[num_steps_ascent];
+    //    for (int i = 0; i < num_steps_ascent; i++)
+    //    {
+    //        discrete_time_ascent[i] = dt * (float) (i-1);
+    //    }
+    //    Vector3 F_drag = new Vector3(0,0,0); 
+    //    Vector3 psi_tot = new Vector3(0,0,0);
+    //    // time stepping loop during ascent
+    //    foreach (float dt_i in discrete_time_ascent) 
+    //    {
+    //        foreach (Ejecta e in es)
+    //        {
+    //            F_drag = 0.5f * Mathf.PI * Mathf.Pow(R_r, 2) * coeff_drag(R_r, rho_a, e.v, v_a, mu_a) * 
+    //                (v_a - e.v).magnitude * (v_a - e.v);
+    //            psi_tot = F_drag + F_gr;
+    //            //e.v = e.v + dt / m_r * psi_tot;
+    //            //e.pos = e.pos + dt * e.v;
+    //
+    //            // TODO: use fixedUpdate method
+    //        }
+    //
+    //    }
+    //
+    //    /* EXPLOSION!!! */
+    //    Vector3 blast_origin = es[0].pos;
+    //    float deltav = Mathf.Sqrt((float)(2 * eta * H_e * m_e / m_p));   // change in speed after detonation
+    //
+    //
+    //}
+    //
+    //private float coeff_drag(float radius, float rho_a, Vector3 v, Vector3 v_a, float mu_a)
+    //{
+    //    float cd;
+    //    float Re = (2f * radius * rho_a * (v - v_a).magnitude) / mu_a;
+    //    if (Re > 2e6f)
+    //    {
+    //        cd = 0.18;
+    //    } else if (Re <= 2e6f && Re > 3e5f)
+    //    {
+    //        cd = 3.66e-4f * Mathf.pow(Re, 0.4275f);
+    //    } else if (Re <= 3e5f & Re > 400.f)
+    //    {
+    //        cd = 0.5f;
+    //    } else if (Re <= 400f && Re > 1f)
+    //    {
+    //        cd = 24f * Mathf.pow(Re, -.646f);
+    //    } else if (Re < 1f)
+    //    {
+    //        cd = 24.f / Re;
+    //    }
+    //    return cd;
+    //}
 }
--- RomanCandles/Assets/CustomScripts/Shaders/NewUnlitShader.shader
+++ RomanCandles/Assets/CustomScripts/Shaders/NewUnlitShader.shader
@@ -60,6 +60,18 @@ Shader "Unlit/NewUnlitShader"
             float lIntensity;
             float lRadius;
 
+            Texture3D<float4> EHash;
+            float4 binsPerAxis;
+            float4 gridSize;
+            float4 gridMin;
+            float binLength;
+
+            float4 sunDir;
+            float4 sunColor;
+            float sunIntensity;
+
+            float4 smokeColor;
+
             float2 rayBoxIntersect(float3 bMin, float3 bMax, float3 ro, float3 rd) {
                 //calculate intersects for each axis-alligned slab
                 float3 t0 = (bMin - ro) / rd;
@@ -81,18 +93,50 @@ Shader "Unlit/NewUnlitShader"
                 //TODO replace with actuall function
                 //float2 noise = (frac(sin(dot(uv, float2(12.9898, 78.233) * 2.0)) * 43758.5453));
                 //return min(1, abs(noise.x + noise.y) * 0.5);
-                p = p * smokeScale * 0.01;
-                float4 samp = Shape.SampleLevel(samplerShape, p,0);
-                return samp.x;
+
+                //p = p * smokeScale * 0.01;
+                //float4 samp = Shape.SampleLevel(samplerShape, p,0);
+                //return samp.x;
+
                 //uint3 pos = uint3(uint(floor(p.x * 30)) % 256, uint(floor(p.y * 30)) % 256, uint(floor(p.z * 30)) % 256);
                 //float4 samp = Shape[pos];
                 //float density = max(0, samp.r);
                 //return density;
 
-                //return max(0, sin(p.x) + sin(p.y * 0.5) + sin(p.z * 0.1))/3.0;
+                return max(0, sin(p.x) + sin(p.y * 0.5) + sin(p.z * 0.1))/3.0;
+            }
+
+            int3 binCoord(float3 pos) {
+                float3 relativePos = pos - gridMin.xyz;
+                int3 posI = int3(floor(pos / binLength));
+                return posI;
+            }
+
+            float3 colorToCoord(float4 c) {
+                float3 t = float3(c.x * gridSize.x, c.y * gridSize.y, c.z * gridSize.z);
+                return (t + gridMin.xyz);
+            }
+
+            float3 ClosestLight(float3 pos) {
+                int3 b = binCoord(pos);
+                float4 closest = float4(-1, -1, -1, -1);
+                for (int i = b.x - 2; i <= b.x + 2; i++) {
+                    for (int j = b.y - 2; j <= b.y + 2; j++) {
+                        for (int k = b.z - 2; k <= b.z + 2; k++) {
+                            int3 p = int3(i, j, k);
+                            if (p.x < 0 || p.y < 0 || p.z < 0 || p.x > gridSize.x || p.y > gridSize.y || p.z > gridSize.z) continue;
+                            if (EHash[p].w == 0) continue;
+                            if (closest.w < 0 || distance(colorToCoord(EHash[p]), pos) < distance(closest.xyz, pos)) {
+                                closest = float4(colorToCoord(EHash[p]),0);
+                            }
+                        }
+                    }
+                }
+                return closest.xyz;
             }
 
             float3 zeroLight(float3 ro, float3 rd, float stepSize) {
+                float3 lPos = ClosestLight(ro);
                 float3 rl = normalize(lPos - ro);
                 float distToLight = length(lPos - ro);
                 float rcos = max(0, dot(rd, rl)) * distToLight;
@@ -114,7 +158,10 @@ Shader "Unlit/NewUnlitShader"
             }
 
             //return estimate of how much light reaches a given point
-            float3 lightMarch(float3 ro) {
+            float3 ejectaMarch(float3 ro) {
+                float3 lPos = ClosestLight(ro);
+                if (lPos.x < 0) return float3(0, 0, 0);
+                //return float3(0, 1, 1);
                 float3 rd = normalize(lPos - ro);
                 float distToLight = length(lPos - ro);
                 float2 smokeBoxI = rayBoxIntersect(BoundsMin, BoundsMax, ro, rd);
@@ -132,6 +179,24 @@ Shader "Unlit/NewUnlitShader"
                 return (lColor * transmit * lIntensity / (distToLight * distToLight));
             }
 
+            float3 sunMarch(float3 ro) {
+                float maxD = rayBoxIntersect(BoundsMin, BoundsMax, ro, sunDir).y;
+                float stepSize = maxD / numStepsLight;
+                float t = 0;
+                float totalDensity = 0;
+                while (t < maxD) {
+                    float3 p = ro + (sunDir * t);
+                    totalDensity += sampleDensity(p);
+                    t += stepSize;
+                }
+                float transmit = exp(-stepSize * totalDensity);
+                return sunColor.xyz * transmit * sunIntensity;
+            }
+
+            float3 lightMarch(float3 ro) {
+                return ejectaMarch(ro) + sunMarch(ro);
+            }
+
             //returns color of smoke
             float4 rayMarch(float3 ro, float3 rd, float maxD, float stepSize, float4 b) {
 
@@ -143,13 +208,13 @@ Shader "Unlit/NewUnlitShader"
                     float3 pointLight = lightMarch(p);
                     float pointDensity = sampleDensity(p);
                     transmit *= exp(-stepSize * pointDensity * smokeLightAbsorb);
-                    smokeDiffuse += pointDensity * pointLight * transmit;
+                    smokeDiffuse += pointDensity * pointLight * transmit * stepSize;
                     smokeDiffuse += zeroLight(p, rd, stepSize) * transmit;
                     
                     if (transmit < 0.01) break;
                     t += stepSize;
                 }
-                return (b * transmit) + float4(smokeDiffuse, 0.0);
+                return (b * transmit) + (float4(smokeDiffuse, 0.0) * smokeColor);
             }
 
             fixed4 frag(v2f i) : SV_Target
