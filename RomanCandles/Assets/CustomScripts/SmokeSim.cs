6deda06f688a3c2d9eedfada486597272da3a18e
Anthony <anthonyvillegas@berkeley.edu>
2022-05-02 02:23:40 -0700

    Added applyBuoyancy script. Advection kinda working but coordanites stuff still off. ApplyDensity still not working. Once coordinate stuff is figured out for apply density then rest of scripts should easily follow

--- RomanCandles/Assets/CustomScripts/SmokeSim.cs
+++ RomanCandles/Assets/CustomScripts/SmokeSim.cs
@@ -6,17 +6,20 @@ public class SmokeSim : MonoBehaviour
 {
     public RenderTexture[] smokeDensity;
     public RenderTexture[] velocity;
-    public ComputeShader impulseShader;
-    public ComputeShader advectShader;
+    public RenderTexture[] temperature;
+    public RenderTexture[] pressure;
+    public ComputeShader impulseShader, advectShader, buoyancyShader;
+
     public static int READ = 0;
     public static int WRITE = 1;
     public Vector4 textureSize;
     public int size;
 
-    public float impulseRadius;
+    public float impulseRadius, impulsePower;
     public Vector4 impulsePosition;
     
-    public float impulsePower;
+
+    public float buoyancy, tempAmbient, k, dissipation;
 
     //other buffer variables go here
     // Start is called before the first frame update
@@ -31,6 +34,8 @@ public class SmokeSim : MonoBehaviour
 
         smokeDensity = new RenderTexture[2];
         velocity = new RenderTexture[2];
+        temperature = new RenderTexture[2];
+        pressure = new RenderTexture[2];
 
         for (int i = 0; i < smokeDensity.Length; i++){
             smokeDensity[i] = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
@@ -46,31 +51,45 @@ public class SmokeSim : MonoBehaviour
             velocity[i].enableRandomWrite = true;
             velocity[i].wrapMode = TextureWrapMode.Clamp;
             velocity[i].Create();
+
+            temperature[i] = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+            temperature[i].dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+            temperature[i].volumeDepth = size;
+            temperature[i].enableRandomWrite = true;
+            temperature[i].wrapMode = TextureWrapMode.Clamp;
+            temperature[i].Create();
+
+            
+            pressure[i] = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+            pressure[i].dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+            pressure[i].volumeDepth = size;
+            pressure[i].enableRandomWrite = true;
+            pressure[i].wrapMode = TextureWrapMode.Clamp;
+            pressure[i].Create();
+            
         }
-        //time = 0.0f;
-        impulsePosition = new Vector4(9.0f, 9.0f, 13.0f, 0.0f);
-        impulseRadius = 7.0f;//6.0f;
-        impulsePower = 20.0f;//40.0f;
-        for (int i = 0; i < 10; i += 1) {
-            AddDensity(impulsePosition, textureSize, impulseRadius, impulsePower);
-            ApplyForce(impulsePosition, textureSize, impulseRadius, impulsePower, .1f);
-        }
-       
         
+        //debugging values
+        impulsePosition = new Vector4(9.0f, 9.0f, 13.0f, 0.0f);
+        impulseRadius = 7.0f;//90.0f;//7.0f;//6.0f;
+        impulsePower = 20.0f;//40.0f;
+        buoyancy = 10.0f;
+        tempAmbient = 30.0f;
+        dissipation = 0.97f;
+        k = 10000000.0f;
+        AddDensity(impulsePosition, textureSize, impulseRadius, impulsePower);
+        ApplyForce(impulsePosition, textureSize, impulseRadius, impulsePower, .1f);
+        AddTemperature(impulsePosition, textureSize, impulseRadius, impulsePower / 80);
          
     }
 
     void FixedUpdate()
     {     
-        //Un/comment these to show smoke           
-        //float impulseRadius = 90.0f; //debug vals and call
-        //Vector4 impulsePosition = new Vector4(0.0f, 9.0f, 13.0f, 0.0f);
-        //AddDensity(impulsePosition, textureSize, impulseRadius, impulsePower);
-        //ApplyForce(impulsePosition, textureSize, impulseRadius, impulsePower, .1f);
-        float dissipation = 0.95f;
+     
         float dt = Time.deltaTime;
         if(lastUpdate > StepTime) {
-            AdvectDensity(textureSize, dissipation, dt);
+            //AdvectDensity(textureSize, dissipation, dt);
+            //ApplyBuoyancy(buoyancy, tempAmbient, dt);
             lastUpdate = 0.0f;
         }
         lastUpdate += Time.deltaTime;
@@ -91,6 +110,19 @@ public class SmokeSim : MonoBehaviour
         Switch(smokeDensity);
     }
 
+    public void AddTemperature(Vector4 position, Vector4 textureSize, float radius, float power) {
+        int kernelHandle = impulseShader.FindKernel("AddDensity");
+        impulseShader.SetTexture(kernelHandle, "Prev", temperature[READ]);
+        impulseShader.SetTexture(kernelHandle, "Result", temperature[WRITE]);
+        impulseShader.SetFloat("radius", radius);
+        impulseShader.SetFloat("power", power);
+        impulseShader.SetVector("position", position);
+        impulseShader.SetVector("size", textureSize);
+        impulseShader.Dispatch(kernelHandle, (int) size / 8, (int) size / 8, (int) size / 8);
+        Switch(temperature);
+    }
+
+
     public void ApplyForce(Vector4 position, Vector4 textureSize, float radius, float force, float dt) {
         int kernelHandle = impulseShader.FindKernel("ApplyForce");
         impulseShader.SetTexture(kernelHandle, "Prev", velocity[READ]);
@@ -101,6 +133,21 @@ public class SmokeSim : MonoBehaviour
         impulseShader.SetVector("position", position);
         impulseShader.SetVector("size", textureSize);
         impulseShader.Dispatch(kernelHandle, (int) size / 8, (int) size / 8, (int) size / 8);
+        Switch(velocity);
+    }
+
+    public void ApplyBuoyancy(float buoyancy, float tempAmbient, float dt) {
+        int kernelHandle = buoyancyShader.FindKernel("ApplyBuoyancy");
+        buoyancyShader.SetTexture(kernelHandle, "Prev", velocity[READ]);
+        buoyancyShader.SetTexture(kernelHandle, "Result", velocity[WRITE]);
+        buoyancyShader.SetTexture(kernelHandle, "Temperature", temperature[READ]);
+        buoyancyShader.SetTexture(kernelHandle, "Density", smokeDensity[READ]);
+        buoyancyShader.SetFloat("tempAmbient", tempAmbient);
+        buoyancyShader.SetFloat("buoyancy", buoyancy);
+        buoyancyShader.SetFloat("k", k);
+        buoyancyShader.SetFloat("dt", dt);
+        //impulseShader.SetVector("size", textureSize);
+        buoyancyShader.Dispatch(kernelHandle, (int) size / 8, (int) size / 8, (int) size / 8);
         Switch(velocity);
     }
 
