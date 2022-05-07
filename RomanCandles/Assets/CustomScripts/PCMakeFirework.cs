8929710ca5a111937d8746a59897628e04f59fb5
Daniel He <tcdanielh@berkeley.edu>
2022-05-06 19:29:33 -0700

    fireworks shoot out from hand, and trying to fix my git ignore

--- RomanCandles/Assets/CustomScripts/PCMakeFirework.cs
+++ RomanCandles/Assets/CustomScripts/PCMakeFirework.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 public class PCMakeFirework : MonoBehaviour
 {
     [SerializeField] GameObject fireworkPrefab;
+
+
+    public GameObject leftControllerObj;
+    public GameObject rightControllerObj;
+
+    private InputDevice leftController;
+    private InputDevice rightController;
+
+    private bool leftAlreadyPressed;
+    private bool rightAlreadyPressed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        //Debug.Log(rightController);
     }
 
     // Update is called once per frame
@@ -27,6 +41,52 @@ public class PCMakeFirework : MonoBehaviour
             fsim.color = Random.ColorHSV();
             fsim.playerTraj = dir;
             fsim.playerOrigin = p3;
+        }
+
+        leftController.TryGetFeatureValue(CommonUsages.triggerButton, out bool leftPressed);
+        rightController.TryGetFeatureValue(CommonUsages.triggerButton, out bool rightPressed);
+
+        if (leftPressed && !leftAlreadyPressed)
+        {
+            leftAlreadyPressed = true;
+
+            //Vector2 angle = Random.insideUnitCircle;
+            //angle /= 3;
+            Vector3 dir = leftControllerObj.transform.forward;
+            //dir.Normalize();
+            //Vector2 pos = Random.insideUnitCircle;
+            //pos *= 2;
+            Vector3 p3 = leftControllerObj.transform.position;
+            FireworkSim fsim = Instantiate(fireworkPrefab).GetComponent<FireworkSim>();
+            fsim.color = Random.ColorHSV();
+            fsim.playerTraj = dir;
+            fsim.playerOrigin = p3;
+
+        }
+        else if (!leftPressed && leftAlreadyPressed)
+        {
+            leftAlreadyPressed = false;
+        }
+
+        if (rightPressed && !rightAlreadyPressed)
+        {
+            rightAlreadyPressed = true;
+
+            //Vector2 angle = Random.insideUnitCircle;
+            //angle /= 3;
+            Vector3 dir = rightControllerObj.transform.forward;
+            //dir.Normalize();
+            //Vector2 pos = Random.insideUnitCircle;
+            //pos *= 2;
+            Vector3 p3 = rightControllerObj.transform.position;
+            FireworkSim fsim = Instantiate(fireworkPrefab).GetComponent<FireworkSim>();
+            fsim.color = Random.ColorHSV();
+            fsim.playerTraj = dir;
+            fsim.playerOrigin = p3;
+
+        } else if (!rightPressed && rightAlreadyPressed)
+        {
+            rightAlreadyPressed = false;
         }
     }
 }
